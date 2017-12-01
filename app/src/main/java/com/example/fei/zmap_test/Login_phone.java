65f558379570eb24e33a781d1be43854f8648c5d
BoldFei <1719937412@qq.com>
2017-12-01 09:57:01 +0800

    个人信息登录注册字体颜色更改

--- app/src/main/java/com/example/fei/zmap_test/Login_phone.java
+++ app/src/main/java/com/example/fei/zmap_test/Login_phone.java
@@ -4,41 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class Profile extends AppCompatActivity {
+public class Login_phone extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_login_phone);
         getSupportActionBar().hide();
 
-        ImageButton back = (ImageButton) findViewById(R.id.back);
+        ImageButton back = (ImageButton) findViewById(R.id.login_phone_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
-        TextView login_register = (TextView) findViewById(R.id.login_register_text);
-        login_register.setOnClickListener(new View.OnClickListener() {
+        TextView register = (TextView) findViewById(R.id.login_phone_go_register);
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
+                Intent intent = new Intent(Login_phone.this, Register.class);
+                startActivity(intent);
+                finish();
             }
         });
-        ImageButton me = (ImageButton) findViewById(R.id.show_me);
-        me.setOnClickListener(new View.OnClickListener() {
+
+        //按钮目前只有返回功能
+        Button login_button = (Button) findViewById(R.id.login_phone_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
+                finish();
             }
         });
-    }
-    public void login(){
-        Intent intent = new Intent(Profile.this, Login_phone.class);
-        startActivity(intent);
     }
 }
