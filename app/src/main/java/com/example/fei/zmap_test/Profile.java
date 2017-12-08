276e43c9c82976f561009002c4a2e728a70c8c27
d o <d o>
2017-12-08 16:56:26 +0800

    ido 修改默认为账号密码登陆

--- app/src/main/java/com/example/fei/zmap_test/Profile.java
+++ app/src/main/java/com/example/fei/zmap_test/Profile.java
@@ -1,11 +1,9 @@
 package com.example.fei.zmap_test;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Profile extends AppCompatActivity {
@@ -85,7 +83,7 @@ public class Profile extends AppCompatActivity {
         }
     }
     public void login(){
-        Intent intent = new Intent(Profile.this, LoginPhone.class);
+        Intent intent = new Intent(Profile.this, LoginAccount.class);
         startActivity(intent);
     }
 }
